dfe2ce7abf281e27aa6473b07f4d1abf9161bda3
Yujin <yujin.mar.chung@gmail.com>
2024-11-11 17:53:49 +0900

    MediaQuery.of(context)에서 얻은 width로 이미지 꽉 채우기

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: StorePage() // Material 앱 내에서 가장 처음 실행 될 페이지 선언~
-        );
+    return MaterialApp(
+      home: StorePage(),
+      debugShowCheckedModeBanner: false, // Material 앱 내에서 가장 처음 실행 될 페이지 선언~
+    );
   }
 }
 
@@ -18,6 +20,55 @@ class StorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // store page의 UI 요소 구현
-    return Scaffold(); // 각 페이지에는 스캐폴드가 있어야 한다~
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 70,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text('Woman',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Spacer(),
+                    Text('Kids',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Spacer(),
+                    Text('Shoes',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Spacer(),
+                    Text('Bag',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Image.asset(
+                "assets/bag.png",
+                fit: BoxFit.fill,
+                width: MediaQuery.of(context).size.width,
+              ),
+            ),
+            Expanded(
+                child: Image.asset(
+              "assets/shoes.png",
+              fit: BoxFit.fill,
+              width: MediaQuery.of(context).size.width,
+            )),
+          ],
+        ),
+      ),
+    ); // 각 페이지에는 스캐폴드가 있어야 한다~
   }
 }
